9e8c41f7e434ff9b13e2498774476949ac634112
Step174 <itstep9328@gmail.com>
2017-02-14 15:52:48 +0500

    карта на JS

--- js/modal-content.js
+++ js/modal-content.js
@@ -9,14 +9,14 @@ var storage = localStorage.getItem("login");
 active.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.add("modal-content-show");
-    
+
     if (storage) {
-    	login.value = storage;
-    	password.focus();
+        login.value = storage;
+        password.focus();
     } else {
-    	login.focus();
+        login.focus();
     }
-  
+
 });
 
 close.addEventListener("click", function(event) {
@@ -26,22 +26,47 @@ close.addEventListener("click", function(event) {
 });
 
 form.addEventListener("submit", function(event) {
-	 event.preventDefault();
-        if (!login.value || !password.value) {
-          event.preventDefault();
-          popup.classList.remove("modal-error");
-          popup.offsetWidth = popup.offsetWidth;
-          popup.classList.add("modal-error");
-        } else {
-          localStorage.setItem("login", login.value);
-        }
-      });
+    event.preventDefault();
+    if (!login.value || !password.value) {
+        event.preventDefault();
+        popup.classList.remove("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
+    } else {
+        localStorage.setItem("login", login.value);
+    }
+});
 
 window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-  	if (popup.classList.contains("modal-content-show")) {
-  		popup.classList.remove("modal-content-show");
-  		popup.classList.remove("modal-error");
-  	}
-  }
+    if (event.keyCode === 27) {
+        if (popup.classList.contains("modal-content-show")) {
+            popup.classList.remove("modal-content-show");
+            popup.classList.remove("modal-error");
+        }
+    }
+});
+
+
+
+var mapOpen = document.querySelector(".js-open-map");
+
+var mapPopup = document.querySelector(".modal-content-map");
+var mapClose = mapPopup.querySelector(".modal-content-close");
+
+mapOpen.addEventListener("click", function(event) {
+    event.preventDefault();
+    mapPopup.classList.add("modal-content-show");
+});
+
+mapClose.addEventListener("click", function(event) {
+    event.preventDefault();
+    mapPopup.classList.remove("modal-content-show");
+});
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        if (mapPopup.classList.contains("modal-content-show")) {
+            mapPopup.classList.remove("modal-content-show");
+        }
+    }
 });
